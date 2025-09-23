a4a85eae0c3926e23de5c668efade5364ad4f7b0
shubhangi kore <koreshubhangi291@gmail.com>
2025-09-23 22:43:58 +0530

    the port is updated

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // backend URL
+  baseURL: "http://localhost:3000", // backend URL
 });
 
 export default API;
